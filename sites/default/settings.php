623eb262c3e3671b580173aaf5c389b136d322e0
Kyle Pratuch <kyle.pratuch@gmail.com>
2015-10-26 14:42:46 -0700

    add simple_contact module

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -213,15 +213,15 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'cooking_blog_database',
       'username' => 'cooking_admin',
       'password' => 'cookingdrupaldrupal',
-      'host' => 'localhost',
-      'port' => '',
+      'host' => '127.0.0.1',
+      'port' => '8889',
       'driver' => 'mysql',
       'prefix' => '',
     ),
